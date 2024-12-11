358bd9b299053f7bb032e74d33c0b0a94d238fde
MuriloMorais2021 <murilo2015morais@gmail.com>
2024-12-10 22:19:02 -0300

    First Commit

---
+++ app/login/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Index (){
+    return (
+        <>
+            <h1>Tela de Login:</h1>
+        </>
+    );
+}
---
+++ app/ui/login/login.tsx
@@ -0,0 +1,14 @@
+import '@/app/ui/global.css';
+
+const Index = () => {
+
+    return (
+        <>
+            <div>
+                <h1>Pagina de login </h1>
+            </div>
+        </>
+    );
+}
+
+export { Index }
